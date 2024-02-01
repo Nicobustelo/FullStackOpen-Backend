7503a9d3deceeab2611319bda7ecbac4e5464977
Nico Bustelo <nbustelo2002@gmail.com>
2024-02-01 17:48:10 -0300

    Exercises 4.8-4.12 finished

--- part4/blogList-backend/test/blogs_api.test.js
+++ part4/blogList-backend/test/blogs_api.test.js
@@ -1,14 +1,92 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog');
 
 const api = supertest(app);
 
-test('blogs are returned as json', async () => {
+const initialBlogs = [
+	{
+		title: 'La odisea de la vagancia',
+		author: 'Nico Bustelo',
+		url: 'example.com',
+		likes: 50,
+	},
+	{
+		title: 'Nuevo blog sin titulo',
+		author: 'Algun autor',
+		url: 'example2.com',
+		likes: 100,
+	},
+];
+
+beforeEach(async () => {
+	await Blog.deleteMany({});
+	let blogObject = new Blog(initialBlogs[0]);
+	await blogObject.save();
+	blogObject = new Blog(initialBlogs[1]);
+	await blogObject.save();
+});
+
+test('returns correct amount of blog post', async () => {
+	const response = await api.get('/api/blogs');
+	expect(response.body).toHaveLength(2);
+});
+
+test('returns blogs in JSON format', async () => {
+	await api.get('/api/blogs').expect('Content-Type', /application\/json/);
+});
+
+test('Unique identifier of blog post is named "id"', async () => {
+	const response = await api.get('/api/blogs');
+
+	expect(response.body[0].id).toBeDefined();
+});
+
+test('HTTP POST successfully creates a new blog post', async () => {
+	const newBlog = {
+		title: 'Blog de prueba en test',
+		author: 'Tester',
+		url: 'test.com.ar',
+		likes: 80,
+	};
+
 	await api
-		.get('/api/blogs')
-		.expect(200)
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
 		.expect('Content-Type', /application\/json/);
+
+	const response = await api.get('/api/blogs');
+
+	expect(response.body).toHaveLength(initialBlogs.length + 1);
+});
+
+test('likes property is missing from the request, default value should be 0', async () => {
+	const newBlog = {
+		title: 'Blog de prueba en test sin likes property',
+		author: 'Tester without likes',
+		url: 'test.com.ar',
+	};
+
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/);
+
+	const result = await api.get('/api/blogs');
+	expect(result.body).toHaveLength(initialBlogs.length + 1);
+	expect(result.body[initialBlogs.length].likes).toBe(0);
+});
+
+test('if title or url properties are missing, backend responds with status code 400 Bad Request', async () => {
+	const newNote = {
+		author: 'Tester without title or url',
+		likes: 80,
+	};
+
+	await api.post('/api/blogs').send(newNote).expect(400);
 });
 
 afterAll(async () => {
--- part4/blogList-backend/utils/config.js
+++ part4/blogList-backend/utils/config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const mongoURL =
 	process.env.NODE_ENV === 'test'
-		? process.env.mongoURL
-		: process.env.mongoURLtest;
+		? process.env.mongoURLtest
+		: process.env.mongoURL;
 const PORT = process.env.PORT;
 
 module.exports = {
